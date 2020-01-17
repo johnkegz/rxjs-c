4f689152a4f31ffc1e67f3f06cc4a127dac5bd7c
johnkegz <johnkal24@gmail.com>
2020-01-17 15:03:18 +0300

    continue with hot observable

--- src/code.ts
+++ src/code.ts
@@ -1,34 +1,13 @@
 import { Observable } from "rxjs/Observable";
-import "rxjs/add/operator/share"
+import { fromEvent  } from 'rxjs/Observable/fromEvent'
 
-var observable = Observable.create((observer:any) => {
-    try{
-    observer.next('Hey guys~')
-    observer.next('How are you')
-    setInterval(()=>{
-        observer.next('i am good')
-    }, 2000)
-
-    }catch(err){
-        observer.error(err)
-    }
-}).share();
-
-var observer = observable.subscribe(
-    (x:any) => addItem(x),
-    (error:any) => addItem(error),
-    () => addItem("completed"),
-    )
-
-
-
-// observer.add(observer2)
+var observable = fromEvent(document, 'mousemove')
 
 setTimeout(() => {
-    var observer2 = observable.subscribe(
-        (x:any) => addItem("SUbscriber 2: " +x),
-        )
-}, 1000)
+    var subscriptions = observable.subscribe(
+        (x:any) => addItem(x)
+    )
+}, 2000);
 
 function addItem(val:any) {
     var node = document.createElement("li");
