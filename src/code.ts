97a3471a4191a30caeea7f9896e21cbfe4c2c941
johnkegz <johnkal24@gmail.com>
2020-01-17 15:35:02 +0300

    add window time on ReplaySubject

--- src/code.ts
+++ src/code.ts
@@ -1,25 +1,21 @@
 import { ReplaySubject } from "rxjs/ReplaySubject";
 
-var subject = new ReplaySubject(2);
+var subject = new ReplaySubject(30, 500);
 
 subject.subscribe(
     (data:any) => addItem("observer 1: "+data),
     (error:any) => addItem(error),
     () => addItem("observer one completed !"),
 )
-subject.next("the first thing has been sent")
-subject.next("another thing has been sent")
-subject.next(">>>>>>>>>>the second observer is about to start .....")
 
-var observer2 = subject.subscribe(
-    (data:any) => addItem("observer 2: "+data)
-)
+var i = 1;
+var int = setInterval(()=> subject.next(i++), 100)
 
-subject.next("the second thing has been sent")
-subject.next("the third thing has been sent")
-
-observer2.unsubscribe()
-subject.next("the final thing has been sent")
+setTimeout(()=>{
+    var observer2 = subject.subscribe(
+        (data:any) => addItem("observer 2: "+data)
+    )
+}, 500)
 
 function addItem(val:any) {
     var node = document.createElement("li");
